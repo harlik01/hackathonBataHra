04555a0a09d8cd31bacd6b759ff1012e4cbc1f0e
harlik01 <103099312+harlik01@users.noreply.github.com>
2022-11-04 23:27:28 +0100

    Update cards.js

--- cards.js
+++ cards.js
@@ -44,24 +44,17 @@ let gameCards = [
     new Card("imgs/example.png", "Josef", "Člen městské rady", "Škola je příliš nákladná – zrušme ji.", [20, -10, 25, 10], [-25, 10, -35, 10], true, 3, 2),
     new Card("imgs/example.png", "Josef", "Člen městské rady", "Město Vám nabízí pozemky na výstavbu budov. Máte zájem?", [-10, 10, 0, -20], [15, -15, 0, 20], false, 3, 0),
     new Card("imgs/example.png", "Honza", "Správce kina", "Kino potřebuje rekonstrukci", [-15, 10, -5, -10], [15, -20, 10, 10], false, 2, 1),
-    new Card("imgs/example.png", "Emil", "Sportovní nadšenec", "Ve městě chybí sport. Založte fotbalový klub.", [-15, 10, 0, 0], [15, -20, 0, 5], true, 2, 1),
+    new Card("imgs/example.png", "Emil", "Sportovní nadšenec", "Ve městě chybí sport. Založte fotbalový klub.", [-15, 10, 0, 0], [15, -20, 0, 5], true, 2, 1)
+    ];
 
-<<<<<<< HEAD
-
-
-
-
-];
-=======
-let rarityQuantity = [[], [], [], [], []];
-for(let i = 0; i < 5; i++) {
-    for(let x = 0; x < gameCards.length; x++) {
-        if(gameCards[x].rarity == (i + 1)) {
-            rarityQuantity[i].push(x);
+    let rarityQuantity = [[], [], [], [], []];
+    for(let i = 0; i < 5; i++) {
+        for(let x = 0; x < gameCards.length; x++) {
+            if(gameCards[x].rarity == (i + 1)) {
+                rarityQuantity[i].push(x);
+            }
         }
     }
-}
-
-// First card
-nextCard();
->>>>>>> 779edd1b70bb38ac745cc099398af61fbd6561dc
+    
+    // First card
+    nextCard();
