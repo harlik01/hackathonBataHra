941924c1d0f8e0e700c57e6ba73fdd7e290fb37f
harlik01 <103099312+harlik01@users.noreply.github.com>
2022-11-04 10:38:09 +0100

    Add card.js + game.js

---
+++ card.js
@@ -0,0 +1,9 @@
+class Card {
+    constructor(imgPath, text, bgColor, optionYes, optionNo) {
+        this.imgPath = imgPath;
+        this.text = text;
+        this.bgColor = bgColor;
+        this.optionYes = optionYes;
+        this.optionNo = optionNo;
+    }
+}
---
+++ game.js
@@ -0,0 +1,8 @@
+class Game {
+    constructor() {
+        this.moralka = 50;
+        this.penize = 50;
+        this.vzdelavani = 50;
+        this.technologie = 50;
+    }
+}
