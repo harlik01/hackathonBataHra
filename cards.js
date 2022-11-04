2c248073b11739ce80584da0efef7556d66ead43
MatyasD <matyasdrobik@seznam.cz>
2022-11-04 19:01:57 +0100

    Added objects

--- cards.js
+++ cards.js
@@ -1,1 +1,37 @@
-let card1 = new Card("imgs/example.png", "Mám manželku a 9 dětí, musím je živit. Potřebuji dostat přidáno.", "blue", [-15, 5, 0, 0], [20, -15, 0, 0]);
+let card1 = new Card("imgs/example.png", "Klement", "Zaměstnanec", "Mám manželku a 9 dětí, musím je živit. Potřebuji dostat přidáno.", "blue", [-15, 5, 0, 0], [20, -15, 0, 0], 2, 0);
+
+let card2 = new Card("imgs/example.png", "Klement", "Zaměstnanec", "Zvyšuje se inflace a stěží  vyjdu s penězi.", "blue", [-10, 0, 0, 0], [15, -10, 0, 0], 5, 1);
+
+let card3 = new Card("imgs/example.png", "Klement", "Zaměstnanec", "Již tady nějakou dobu pracuji, nemohl bych dostat přidáno?", "blue", [-10, 0, 0, 0], [10, -10, 0, 0], 4, 3);
+
+let card4 = new Card("imgs/example.png", "Marie", "Zaměstnanec", "Ženy nemají stejné platové podmínky jako muži!", "blue", [-20, 5, 0, 0], [20, -15, 0, 0], 1, 0);
+
+let card5 = new Card("imgs/example.png", "Marie", "Zaměstnanec", "Firma se rozrůstá, ale plat mám pořád stejný. Zvažuji výpověď.", "blue", [-10, 10, 0, 0], [10, -10, 0, 0], 3, 3);
+
+let card6 = new Card("imgs/example.png", "Marie", "Zaměstnanec", "Právě se mi narodilo dítě, mohu dostat placenou dovolenou?", "blue", [-20, 10, 0, 0], [20, -20, 0, 0], 2, 4);
+
+let card7 = new Card("imgs/example.png", "Karel Schinzel", "Vynálezce", "Dobrý den, snažím se rozšířit barevnou fotografii a mám pro Vás nabídku. Můžu jimi vylepšit vaše instruktážní materiály pro zaměstnance.", "blue", [-10, 0, -10, 0], [10, -10, 20, 0], 1, 1);
+
+let card8 = new Card("imgs/example.png", "Otto Wichterle", "Vynálezce", "Zdravím, pane řediteli, můžu usnadnit práci Vašim zaměstnancům. Kontaktní čočky jim zpříjemní život. ", "blue", [-10, 0, -10, 0], [10, -10, 0, 0], 1, 3);
+
+let card9 = new Card("imgs/example.png", "František Gahura", "Architekt", "Dobrý den, zaměstnanci si stěžují na nedostupnou zdravotní péčí. Mám pro Vás plány nové nemocnice. ", "blue", [-20, 0, 0, -20], [25, -15, 0, 20], 1, 2);
+
+let card10 = new Card("imgs/example.png", "František Gahura", "Architekt", "Zdravím, šéfe, rozšiřujete firmu a zaměstnaci nemají kde bydlet. Postavte jim nové domky.", "blue", [-20, -10, 0, -15], [15, -10, 0, 10], 3, 2);
+
+let card11 = new Card("imgs/example.png", "František Gahura", "Architekt", "Dobrý den, mám pro Vás návrhy nového obchodního domu. Chcete jej vystavět?", "blue", [-10, 0, 0, -10], [10, -15, 0, 15], 2, 1);
+
+let card12 = new Card("imgs/example.png", "František Gahura", "Architekt", "Zdravím, ve Zlíně chybí kino, mám hotový návrh, ale musíme zničit dva domky. ", "blue", [0, -10, -10, -5], [0, -15, 0, 10], 2, 4);
+
+let card13 = new Card("imgs/example.png", "Jan Jánský", "Lékař", "Dobrý den, ve firmě Vám chybí lékař, měl bych zájem. ", [-10, 0, 0, 0], [10, -10, 0, 0], 5, 0);
+
+let card14 = new Card("imgs/example.png", "Jan Jánský", "Lékař", "Zdravím, chtěl bych ve Vaší nemocnici představit můj nový objev. ", [-10, 0, -10, 0], [10, 0, 10, 0], 5, 5);
+
+let card15 = new Card("imgs/example.png", "Josef Ressel", "Vynálezce", "Zdravím, pane Baťo,  vytvořil jsem nový stroj, který nahradí některé zaměstnance. ", [0, -5, 0, -10], [-10, -10, 0, 10], 5, 3);
+
+let card16 = new Card("imgs/example.png", "Josef Ressel", "Vynálezce", "Dobrý den, mám pro Vás návrh výstavby vodního kanálu. ", [-5, 0, 0, -10], [10, -10, 0, 10], 5, 2);
+
+let card17 = new Card("imgs/example.png", "František Křižík", "Vynálezce", "Dobrý den, pane Baťo, navrhuji výstavbu osvětlení ve výrobě, ale osvětlovači přijdou o práci. ", [0, 0, 0, -10], [10, -10, 0, 10], 5, 0);
+
+let card18 = new Card("imgs/example.png", "František Křižík", "Vynálezce", "Dobrý den, lidé nejsou spokojeni s noční podobou města, chcete vystavět pouliční lampy?", [-10, 0, 0, -10], [10, -10, 0, 15], 3, 1);
+
+let card19 = new Card("imgs/example.png", "Ámos", "Učitel", "Lidé jsou nespokojeni s poskytovaným vzdělaním. Navrhuji výstavbu školy práce.", [-10, 0, -25, -15], [20, -20, 25, 15], 4, 0);
