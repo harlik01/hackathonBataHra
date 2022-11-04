24232382a53726a965ac0c909cf43808a8cbe28c
harlik01 <103099312+harlik01@users.noreply.github.com>
2022-11-04 12:30:44 +0100

    swipejs

---
+++ script.js
@@ -0,0 +1,12 @@
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+    startSlide: 0,
+    speed: 400,
+    auto: 3000,
+    draggable: true,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    ignore: ".scroller",
+    callback: function(index, elem, dir) {},
+    transitionEnd: function(index, elem) {}
+  });
